dbe9f4aed69aa88e1ccc372f3d4bbb4f89948cbc
Dominik <dominik@my-account.net>
2021-06-26 13:31:03 +0200

    stringify ip version correctly

--- pkg/recorder/handler.go
+++ pkg/recorder/handler.go
@@ -3,8 +3,11 @@ package recorder
 import (
 	"context"
 	"github.com/enriquebris/goconcurrentqueue"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/schidstorm/nftables-exporter/pkg/metrics"
+	nftables_exporter "github.com/schidstorm/nftables-exporter/pkg/nftables-exporter"
 	"github.com/sirupsen/logrus"
 	"strconv"
 )
@@ -39,7 +42,7 @@ func CreateHandler(ctx context.Context, payloadQueue goconcurrentqueue.Queue) {
 			"oif":      metric.OutputInterface,
 			"saddr":    metric.SourceIp,
 			"dport":    "",
-			"ipVersion": string(rune(metric.IpVersion)),
+			"ipVersion": strconv.Itoa(metric.IpVersion),
 			"group":    strconv.Itoa(handlerContext.group),
 			"host":     handlerContext.host,
 			"protocol": metric.Protocol,
@@ -66,4 +69,63 @@ func CreateHandler(ctx context.Context, payloadQueue goconcurrentqueue.Queue) {
 
 
 	}
+}
+
+func ParsePayload(payload PayloadInterface) *metrics.PacketMetric {
+	metric := &metrics.PacketMetric{}
+
+	if inInterface, err := nftables_exporter.GetInterfaceFromNumber(payload.GetInDev()); err == nil {
+		metric.InputInterface = inInterface.Attrs().Name
+	} else {
+		logrus.Error(err)
+	}
+
+	if outInterface, err := nftables_exporter.GetInterfaceFromNumber(payload.GetOutDev()); err == nil {
+		metric.OutputInterface = outInterface.Attrs().Name
+	} else {
+		logrus.Error(err)
+	}
+
+	packetV4 := gopacket.NewPacket(payload.GetData(), layers.LayerTypeIPv4, gopacket.NoCopy)
+	if ipLayer := packetV4.Layer(layers.LayerTypeIPv4); ipLayer != nil {
+		ipv4, _ := ipLayer.(*layers.IPv4)
+		if ipv4.Version == 4 {
+			metric.IpVersion = 4
+			metric.SourceIp = ipv4.SrcIP.String()
+			metric.DestinationIp = ipv4.DstIP.String()
+			metric.Protocol = ipv4.Protocol.String()
+		}
+	}
+
+	packetV6 := gopacket.NewPacket(payload.GetData(), layers.LayerTypeIPv6, gopacket.NoCopy)
+	if ipLayer := packetV6.Layer(layers.LayerTypeIPv6); ipLayer != nil {
+		ipv6, _ := ipLayer.(*layers.IPv6)
+		if ipv6.Version == 6 {
+			metric.IpVersion = 4
+			metric.SourceIp = ipv6.SrcIP.String()
+			metric.DestinationIp = ipv6.DstIP.String()
+			metric.Protocol = ipv6.NextHeader.String()
+		}
+	}
+
+	if metric.IpVersion == 0 {
+		return nil
+	}
+
+	if tcpLayer := packetV4.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+		metric.Tcp = true
+		tcp, _ := tcpLayer.(*layers.TCP)
+		metric.DestinationPort = uint16(tcp.DstPort)
+	} else {
+		metric.Tcp = false
+		if udpLayer := packetV4.Layer(layers.LayerTypeUDP); udpLayer != nil {
+			metric.Udp = true
+			udp, _ := udpLayer.(*layers.UDP)
+			metric.DestinationPort = uint16(udp.DstPort)
+		} else {
+			metric.Udp = false
+		}
+	}
+
+	return metric
 }
--- pkg/recorder/packet.go
+++ pkg/recorder/packet.go
@@ -4,11 +4,7 @@ import (
 	"context"
 	"github.com/chifflier/nflog-go/nflog"
 	"github.com/enriquebris/goconcurrentqueue"
-	"github.com/google/gopacket"
-	"github.com/google/gopacket/layers"
-	"github.com/schidstorm/nftables-exporter/pkg/metrics"
 	nftables_exporter "github.com/schidstorm/nftables-exporter/pkg/nftables-exporter"
-	"github.com/sirupsen/logrus"
 )
 
 var handlerContext *handler
@@ -70,62 +66,3 @@ func HandlePayload(p PayloadInterface) int {
 	return 0
 }
 
-func ParsePayload(payload PayloadInterface) *metrics.PacketMetric {
-	metric := &metrics.PacketMetric{}
-
-	if inInterface, err := nftables_exporter.GetInterfaceFromNumber(payload.GetInDev()); err == nil {
-		metric.InputInterface = inInterface.Attrs().Name
-	} else {
-		logrus.Error(err)
-	}
-
-	if outInterface, err := nftables_exporter.GetInterfaceFromNumber(payload.GetOutDev()); err == nil {
-		metric.OutputInterface = outInterface.Attrs().Name
-	} else {
-		logrus.Error(err)
-	}
-
-	packetV4 := gopacket.NewPacket(payload.GetData(), layers.LayerTypeIPv4, gopacket.NoCopy)
-	if ipLayer := packetV4.Layer(layers.LayerTypeIPv4); ipLayer != nil {
-		ipv4, _ := ipLayer.(*layers.IPv4)
-		if ipv4.Version == 4 {
-			metric.IpVersion = 4
-			metric.SourceIp = ipv4.SrcIP.String()
-			metric.DestinationIp = ipv4.DstIP.String()
-			metric.Protocol = ipv4.Protocol.String()
-		}
-	}
-
-	packetV6 := gopacket.NewPacket(payload.GetData(), layers.LayerTypeIPv6, gopacket.NoCopy)
-	if ipLayer := packetV6.Layer(layers.LayerTypeIPv6); ipLayer != nil {
-		ipv6, _ := ipLayer.(*layers.IPv6)
-		if ipv6.Version == 6 {
-			metric.IpVersion = 4
-			metric.SourceIp = ipv6.SrcIP.String()
-			metric.DestinationIp = ipv6.DstIP.String()
-			metric.Protocol = ipv6.NextHeader.String()
-		}
-	}
-
-	if metric.IpVersion == 0 {
-		logrus.Error("Invalid ip packet")
-		return nil
-	}
-
-	if tcpLayer := packetV4.Layer(layers.LayerTypeTCP); tcpLayer != nil {
-		metric.Tcp = true
-		tcp, _ := tcpLayer.(*layers.TCP)
-		metric.DestinationPort = uint16(tcp.DstPort)
-	} else {
-		metric.Tcp = false
-		if udpLayer := packetV4.Layer(layers.LayerTypeUDP); udpLayer != nil {
-			metric.Udp = true
-			udp, _ := udpLayer.(*layers.UDP)
-			metric.DestinationPort = uint16(udp.DstPort)
-		} else {
-			metric.Udp = false
-		}
-	}
-
-	return metric
-}
